3d91bac32447c0cf83cd40c92e32c34910b8dd61
lucasjstn <lucas7justino@gmail.com>
2023-06-30 14:09:22 -0300

    returning errorr confirm your password

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,36 @@
 import User from "../models/userModel.js";
+import validator from "validator";
 
 export const signUp = async (request, response, next) => {
+
+
+  const strongPassword = validator.isStrongPassword(request.body.password, {
+    minLowercase: 1,
+    minUppercase: 0,
+    minSymbols: 0,
+    minNumbers: 1,
+  })
+
+  if(!strongPassword) {
+    response.status(400).json({
+      status: "failed",
+      message: "Please enter a password with minimum 8 characters length, a lower case letter and a number"
+    })
+    
+    return;
+  }
+
+  const password = request.body.password; 
+  const confirmPassword = request.body.confirmPassword;
+
+  if(password !== confirmPassword) {
+    response.status(400).json({
+      status: "failed",
+      message: "Please confirm your password."
+    })
+
+    return;
+  }
 
   const newUser = await User.create(request.body);
 
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcryptjs from "bcryptjs";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import validator from "validator";
 
 dotenv.config({ path: "../../config.env" });
 
@@ -19,19 +20,29 @@ export const userSchema = new mongoose.Schema({
     unique: true,
     lowercase: true,
     minlength: [10, "Your email can't have less than 10 characters."], 
-    maxlength: [30, "Your username can't take more than 30 characters."]
+    maxlength: [30, "Your username can't take more than 30 characters."],
+    validate: [
+      validator.isEmail,
+      "Please enter a valid email."
+    ]
   },
   password: {
     type: String,
     require: [true, "You need to set a password."],
     minlength: [8, "Your password can't have less than 8 characters."], 
-    maxlength: [18, "Your password can't take more than 18 characters."]
+    maxlength: [18, "Your password can't take more than 18 characters."],
   },
   confirmPassword: {
     type: String,
     require: [true, "Please confirm your password"],
     minlength: [8, "Your password can't have less than 8 characters."], 
-    maxlength: [18, "Your password can't take more than 18 characters."]
+    maxlength: [18, "Your password can't take more than 18 characters."],
+    validate: {
+      validator: (item) => {
+        return item === this.password;
+      },
+      message: "Passwords do not match."
+    } 
   }
 })
 

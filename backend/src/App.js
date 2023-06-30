fc1b75177778e34fe5708795abe1ad0b1f6fc948
lucasjstn <lucas7justino@gmail.com>
2023-06-30 15:27:02 -0300

    adding morgan to the project

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,8 +1,11 @@
 import express from "express";
 import authRoute from "./routes/authRoute.js"
+import morgan from "morgan";
+
 export const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 app.use("/api/v1/auth", authRoute);
 
 app.all("*", (req, res, next)=> {

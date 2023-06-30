1a643d273ecffd82f33aa6f0b873c21f7f6b640c
lucasjstn <lucas7justino@gmail.com>
2023-06-29 23:01:39 -0300

    same

--- backend/src/App.js
+++ backend/src/App.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoute from "./routes/authRoute.js"
 export const app = express();
 
+app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 
 app.all("*", (req, res, next)=> {

fb04ed183d460dbfd47a75890eae28ab7b4360ed
lucasjstn <lucas7justino@gmail.com>
2023-06-30 18:12:31 -0300

    global error handling

--- backend/src/App.js
+++ backend/src/App.js
@@ -9,8 +9,26 @@ app.use(morgan('tiny'));
 app.use("/api/v1/auth", authRoute);
 
 app.all("*", (req, res, next)=> {
-  res.status(404).json({
-    status: "failed",
-    message: "Can't find route"
+
+  // res.status(404).json({
+  //   status: "failed",
+  //   message: "Can't find route"
+  // })
+
+  const err = new Error(`Cant find ${req.originalUrl} on the server`);
+
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || "Something went wrong";
+
+  res.status(error.statusCode).json({
+    status: error.statusCode,
+    message: error.message
   })
-})
+});

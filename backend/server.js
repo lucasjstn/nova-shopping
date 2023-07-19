93df616d458bdd772b6808a7f70796e3f7b368d4
lucasjstn <lucas7justino@gmail.com>
2023-07-19 00:29:15 -0300

    refactor: reseting to learn purpose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,2 @@
-import mongoose from "mongoose";;
-import dotenv from "dotenv";
-
-import { app } from "./src/App.js"
-
-mongoose.set("strictQuery", false);
-
-dotenv.config({ path: "./config.env" });
-
-const PORT = process.env.PORT;
-
-mongoose.connect(process.env.DATABASE_CLOUD, { usenewurlparser: true })
-  .then(()=> {
-    console.log("Database connected");
-  })
-  .catch(error => {
-    console.log(error);
-  })
-
-const server = app.listen(PORT, ()=> {
-  console.log(`Server is running at port ${PORT}`);
-});
+import express from "express";
 
